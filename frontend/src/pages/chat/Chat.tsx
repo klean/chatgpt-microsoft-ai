1f84159288ef3c7d809b3b0ecbc37dfa5b22a4b6
Kasper Hansen <kasper@klean.dk>
2025-02-03 14:53:37 +0100

    fix header position

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -865,11 +865,12 @@ const Chat = () => {
                 <h1>Hvad kan jeg hjælpe med?</h1>
               </>
             ) : (
+              <>
+              <div className="topBar">
+                <h1>Få hjælp fra FU</h1>
+                <button onClick={clearChat} className="icon-button --clear --right --primary">Ryd chat</button>
+              </div>
               <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? '40px' : '0px' }} role="log">
-                <div className="topBar">
-                  <h1>Få hjælp fra FU</h1>
-                  <button onClick={clearChat} className="icon-button --clear --right --primary">Ryd chat</button>
-                </div>
                 {messages.map((answer, index) => (
                   <>
                     {answer.role === 'user' ? (
@@ -922,6 +923,7 @@ const Chat = () => {
                 )}
                 <div ref={chatMessageStreamEnd} />
               </div>
+              </>
             )}
 
             <Stack horizontal className={styles.chatInput}>
